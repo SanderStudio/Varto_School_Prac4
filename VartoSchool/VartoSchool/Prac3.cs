72f96fc85e7f15ad94bde8d274e3adbcd2f24c70
SanderStudio <venommax8@gmail.com>
2024-03-19 14:21:29 +0200

    Init Commit

---
+++ VartoSchool/VartoSchool/Prac3.cs
@@ -0,0 +1,97 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlTypes;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace VartoSchool
+//{
+
+//    //enum MonthName
+//    //{
+//    //    January = 1,
+//    //    February,
+//    //    March,
+//    //    Aplril,
+//    //    May,
+//    //    June,
+//    //    July,
+//    //    August,
+//    //    September,
+//    //    October,
+//    //    November,
+//    //    December
+//    //}
+//    //class Prac3
+//    //{
+//    //    static void Main(string[] args)
+//    //    {
+//    //        bool ready = true;
+//    //        while(ready)
+//    //        {
+//    //            Console.WriteLine($"Enter month number");
+
+//    //            if (!int.TryParse(Console.ReadLine(), out int x))
+//    //                Console.WriteLine($"Wrong munber!");
+
+//    //            if(x>0 && x<13)
+//    //            {
+//    //                MonthName selectedMonth;
+//    //                selectedMonth = (MonthName)x;
+
+//    //                Console.WriteLine(selectedMonth);
+//    //            }
+//    //            else
+//    //                Console.WriteLine($"Wrong munber!");
+
+//    //            Console.WriteLine($"Continue?");
+//    //            if(Console.ReadLine() == "y")
+//    //                ready = true;
+//    //            else
+//    //                ready = false;
+//    //        }
+//    //    }
+//    //}
+
+//    enum DayName
+//    {
+//        Monday = 1,
+//        Tuesday,
+//        Wednesday,
+//        Thursday,
+//        Friday,
+//        Saturday,
+//        Sunday
+//    }
+//    class Prac3
+//    {
+//        static void Main(string[] args)
+//        {
+//            bool ready = true;
+//            while (ready)
+//            {
+//                Console.WriteLine($"Enter day number");
+
+//                if (!int.TryParse(Console.ReadLine(), out int x))
+//                    Console.WriteLine($"Wrong munber!");
+
+//                if (x > 0 && x < 8)
+//                {
+//                    DayName selectedDay;
+//                    selectedDay = (DayName)x;
+
+//                    Console.WriteLine(selectedDay);
+//                }
+//                else
+//                    Console.WriteLine($"Wrong munber!");
+
+//                Console.WriteLine($"Continue?");
+//                if (Console.ReadLine() == "y")
+//                    ready = true;
+//                else
+//                    ready = false;
+//            }
+//        }
+//    }
+//}
---
+++ VartoSchool/VartoSchool/Prac4.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace VartoSchool
+{
+    class Prac4
+    {
+        static void Main(string[] args)
+        {
+            //int[] myArray = new int[50];
+
+            //for (int i = 0, j = 0; j < myArray.Length; i += 2, j++)
+            //{
+            //    myArray[j] = i;
+            //    Console.WriteLine($"Element {j}: {i}");
+            //}
+
+
+
+            //bool ready = true;
+            //while (ready)
+            //{
+            //    int numbers = 0;
+
+            //    Console.WriteLine($"Enter array length between 24 and 50");
+            //    if (!int.TryParse(Console.ReadLine(), out int x))
+            //        Console.WriteLine($"Wrong munber!");
+            //    else if (x < 24 || x > 50)
+            //        Console.WriteLine($"Wrong munber!");
+            //    else
+            //    {
+            //        int[] myArray = new int[x];
+
+            //        Random random = new Random();
+
+            //        for (int i = 0; i < myArray.Length; i++)
+            //        {
+            //            myArray[i] = random.Next(0, 101);
+            //            if (myArray[i] % 2 == 0)
+            //            {
+            //                numbers++;
+            //            }
+            //        }
+            //        Console.WriteLine($"Arrays include {numbers} pair numbers");
+
+            //        Console.Write("Елементи масиву з парними значеннями: ");
+            //        for (int i = 0; i < myArray.Length; i++)
+            //        {
+            //            if (myArray[i] % 2 == 0)
+            //            {
+            //                Console.Write(myArray[i] + " ");
+            //            }
+            //        }
+            //        Console.WriteLine();
+
+
+            //        Console.WriteLine($"Continue?");
+            //        if (Console.ReadLine() == "y")
+            //            ready = true;
+            //        else
+            //            ready = false;
+            //    }
+            //}
+
+            bool ready = true;
+            int numbers;
+            int letterNumbers;
+            while (ready)
+            {
+                Console.WriteLine($"Enter your Text");
+                string text = Console.ReadLine();
+                string lowerText = text.ToLower();
+                numbers = 0;
+                letterNumbers = 0;
+
+                Console.WriteLine($"Enter letter you want to find");
+                string letter = Console.ReadLine();
+                string letterLower = letter.ToLower();
+                char lowerLetter = Convert.ToChar(letterLower);
+
+                foreach (char item in lowerText)
+                {
+                    if (item == lowerLetter)
+                    {
+                        letterNumbers++;
+                    }
+                    numbers++;
+                }
+                Console.WriteLine($"Text {text} include: {numbers} symbols");
+                Console.WriteLine($"Found {letterNumbers} letters {letter} in {text}");
+
+                Console.WriteLine($"Continue?");
+                if (Console.ReadLine() == "y")
+                    ready = true;
+                else
+                    ready = false;
+            }
+
+
+            //char[] alphabetArray = new char[26];
+
+            //for (int i = 0; i < alphabetArray.Length; i++)
+            //{
+            //    alphabetArray[i] = (char)('a' + i);
+            //}
+
+            //    Console.WriteLine("Символ\tПорядковий номер в ASCII");
+            //    for (int j = 0; j < alphabetArray.Length; j++)
+            //    {
+            //        Console.WriteLine(alphabetArray[j] + "\t" + (int)alphabetArray[j]);
+            //    }
+        }
+    }
+}
